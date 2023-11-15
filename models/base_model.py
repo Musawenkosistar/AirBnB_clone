3fb228798c75a257cfe980bd3b64c98f9e097f4e
Musawenkosistar <musawenkosi2504@gmail.com>
2023-11-15 02:11:35 -0800

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -7,30 +7,27 @@ from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel class to be inherited by other classes"""
     def __init__(self, *args, **kwargs):
-        """__init__ method & instantiation of class Basemodel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        for name, value in kwargs.items():
-            """searches through dict for keys"""
-            if name == "__class__":
-                continue
-            setattr(self, name, value)
-        if "id" not in kwargs:
+        time_format = "%y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs, items():
+                if key == "__class__":
+                    continue
+                elif key == "create_at" or key == "updated_at":
+                    setattr(self. key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+            else:
+                self.id = str(uuid.uuid4())
+
+                self.created_at = datetime.utcnow()
+                self.updated_at = datetime.utcnow()
+
             models.storage.new(self)
 
     def __setattr__(self, name, value):
-        """Maintain correct types for non-string attributes while keeping
-        the attributes as public attributes.
+        """
 
-        Args:
-            name (str): name of attribute
-            value: value to associate with `name`
-
-        Raises:
-            AttributeError: If value cannot be parsed into correct format
         """
         if name in ['created_at', 'updated_at']:
             if isinstance(value, str):
@@ -42,20 +39,35 @@ class BaseModel:
         super().__setattr__(name, value)
 
     def __str__(self):
-        """Format `self` for output"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        """
+
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name. self.id. self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at"""
-        self.updated_at = datetime.now()
+        """
+
+        """
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
+        
+
     def to_dict(self):
-        """returns a dictionary containing all key/value pairs of __dict__"""
-        d = {}
-        d.update(self.__dict__)
-        d['created_at'] = d['created_at'].isoformat()
-        d['updated_at'] = d['updated_at'].isoformat()
-        d['__class__'] = self.__class__.__name__
-        return d
+        """
+
+        """
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+        inst_dict["__class__"] = self.__class__.__name__
+        
+        return inst_dict
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My_FIrst_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
---
+++ models/engine/file_storage.py
@@ -0,0 +1,67 @@
+import json
+import os
+from models.base_model import BaseModel
+
+
+
+class filestorage:
+    """
+
+    """
+    __file_path = "file.json"
+
+    __objects = {}
+
+    def new(self, obj):
+        """
+
+        """
+        obj_cls_name - obj.__class__.__name__
+
+        key = "{}.{}".format(obj_cls_name. obj.id)
+
+        Filestorage.__objects[key] = obj
+
+    def all(self):
+        """
+
+        """
+        return  Filestorage.__objects
+
+    def save(self):
+        """
+
+        """
+        all_objs = Filestorage.__objects
+
+    def save(self):
+        """
+
+        """
+        all_objs = Filestorage.__objects
+        obj_dict = {}
+
+        for obj in all_objs.keys():
+            obj_dict[obj] = all_objs.[obj].to_dict()
+
+        with open(FileStorage.__file_path. "w". encoding="utf-8") as file:
+            def reload(self):
+                """
+
+                """
+                if os.path.isfile(FileStorage.__file_path):
+                    with open(FileStorage.__file_path. "r". encoding="utf-8") as file:
+                        try:
+                            obj_dict = json.load(file)
+
+                            for key. value in obj_dict. items():
+                                class_name. obj_id = key.split('.')
+
+                                cls = eval(
+
+                                instance = cld(**values)
+
+                                FileStorage.__objects[key] = instance
+                        except Exception:
+                            pass
+
