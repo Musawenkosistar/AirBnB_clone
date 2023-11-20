6ef3a9e36f5319b2c3a599e30b32322b8394b777
Musawenkosistar <musawenkosi2504@gmail.com>
2023-11-20 04:59:56 -0800

    AirBnB_clone

--- console.py
+++ console.py
@@ -5,6 +5,7 @@
 import cmd
 import re
 import shlex
+import ast
 from models.base model import BaseModel
 from models.user import User
 from models import storage
@@ -124,37 +125,80 @@ class HBNBCommand(cmd.Cmd):
 
     def default(self, arg):
         """
+        Default behavior for cmd module when input is invalid
+            usage:
+                <class_name>.<method><("id",{"attr_name": "attr_value", "attr_name": "attr_value"}
 
         """
-        arg_list = arg.split('.') #User.show("12w-241') output: ['User', 'show("12w-241")']
-    
+        # initail input
+        # User.update("87f12", {'first_name': "John", "age": 89})
+        arg_list = arg.split('.') # after split
+        # arg_list - ['User',("87f12", {'first_name': "John", "age": 89})
         # arg_list[0] = 'User'
-        # arg_list[1] = 'show("12w-241")'
+        # arg_list[1] = 'update("87f12", {'first_name': "John", "age": 89})'
+
+    
+        # we save the arg_list index zero as incoming class name
         incoming_class_name - arg_list[0]
 
-        command = arg_list[1].split('(')
-        # command[0] = 'show'
-        # command[1] = '"12w-241")'
+        # again we split arg_list[1] containing the method and attributes by opening brace
+        # to seperate the incoming method name and the accompanying arguments
+        command = arg_list[1].split('(') # after split
+        # command - ['update("87f12", {'first_name': "John", "age": 89})'
+        - command[0] = 'update'
+        # command[1] = '"87f12", {'first_name': "John", "age": 89})'
+        # we save the incoming method name as incoming method
+        incoming_method - command[0]  
 
-        incoming_method - command[0]  # 'show'
+        # now we are left with the id, and dict with attr to update, at command index 1
+        # command[1] - "87f12", {'first_name': "John", "age": 89})
         
-        incoming_xtra_arg - command[1].split(')')[0]  # ['"12w-241"', '']
+        incoming_xtra_arg - command[1].split(')')[0] # after split
+        # now, incoming_xtra_arg becomes the id, attr_name, and attr_value
+        # incoming_xtra_arg = "87f12", {'first_name': "John", "age": 89}'
 
-    
+        # this part was for before the need to modify, when we were expecting
+        # User.update("id", attr_name, attr_value)
+
+        all_args = incoming_xtra_arg.split(',')
+        # so we had to further split by the comma, and pass the args to method
+        # construct for update.method. which gave us this:
+        # obj_id - all_args[0]
+        # attribute_name = all args[1]
+        # attribute_value - all_args[2]
+        # but will not work for this format, which is the current format:
+        # incoming_xtra_arg = '"87f12", {'first_name': "John", "age": 89})'
+        
         method_dict - {
                 'all': self.do_all,
                 'show': self.do_show,
                 'destroy': self.do_destroy,
                 'update': self.do_update
                 'count': self.do_count
-
-        }
+                }
 
         if incoming_method in method_dict.keys():
-            return method_dict[incoming_method]("{}.{}".format(incoming_class_name,
-                incoming_xtra_arg))
+            if incoming_method != "update":
+                return method_dict[incoming_method]("{} {}".format(incoming_class_name,
+                                                                    incoming_xtra_arg))
+            else:
+                obj_id, arg_dict = split_curly_braces(incoming_xtra_arg)
+                try:
+                    if isinstance(arg_dict, str):
+                        attributes = arg_dict
+                        return method_dict[incoming_method]("{} {} {}".format(incoming_class_name
+                                                                              attributes
+                                                                              obj_id))
+                    elif isinstance(arg_dict, str):
+                        dict_attributes = arg_dict
+                        return method_dict[incoming_method]("{} {} {}".format(incoming_class_name
+                                                                              obj_id
+                                                                              dict_attributes))
+                except Exception:
+                    print("** argument missing **")
 
-    print("*** Unknown syntax: ()".format(arg))
+
+    print("*** Unknown syntax: {}".format(arg))
     return False
 
         def do_count(self, arg):
@@ -210,17 +254,40 @@ class HBNBCommand(cmd.Cmd):
                 print("** value missing **")
             else:
                 obj = objects[key]
+                curly_braces = re.search(r"\{(.*?)\}", arg)
 
-                attr_name = commands[2]
-                attr_value = commands[3]
+                if curly_braces:
+                    str_data = curly_braces.group(1)
 
-                try:
-                    attr_value = eva;(attr_value)
-                except Exception:
-                    pass
-                setatrr(obj, attr_name, attr_value)
+                    arg_dict - ast.literal_eval("{" + str_data + "}")
+                    # {"first_name": "Johnson", "age": 490}
+
+                    attribute_names = list(arg_dict.keys())
+                    attribute_values = list(arg_dict.values())
+
+                    attr_name1 = attribute_names[0]
+                    attr_value1 = attribute_value[0]
+
+                    attr_name2 = attribute_names[1]
+                    attr_value2 = attribute_value[1]
+
+                    setattr(obj, attr_name1, attr_value1)
+                    setattr(obj, attr_name1, attr_value2)
+                else:
+
+                    
+                    attr_name = commands[2]
+                    attr_value = commands[3]
+
+                    try:
+                        attr_value = eva;(attr_value)
+                    except Exception:
+                         pass
+                    setatrr(obj, attr_name, attr_value)
 
                 obj.save()
+
+
     def help_quit(self, arg):
         """
 
@@ -235,4 +302,4 @@ class HBNBCommand(cmd.Cmd):
         return True
 
 if __name__ == "__main__":
-    HBNBCommand().cm
+    HBNBCommand().cmdloop()
