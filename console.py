05647ebaa37c86a36e035ac1c8a2b1eeefc18d1d
Musawenkosistar <musawenkosi2504@gmail.com>
2023-11-28 03:48:01 -0800

    AirBnB_clone

--- console.py
+++ console.py
@@ -1,305 +1,210 @@
-#!/usr/bin/python
-"""
-
-"""
+#!/usr/bin/python3
+"""Defines the BnB console."""
 import cmd
 import re
-import shlex
-import ast
-from models.base model import BaseModel
-from models.user import User
+from shlex import split
 from models import storage
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models.base_model import BaseModel
+from models.user import User
 from models.state import State
 from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
-    """
-    
-    """
-    prompt = "(hbnb)"
-    valid_classes = ["BaseModel", "User", "Amenity"
-                     "Place", "Review", "State", "City"]
-    
+    """Defines the command interpreter.
 
+    Attributes:
+        prompt (str): The command prompt.
+    """
 
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     def emptyline(self):
-        """
-        
-        """
+        """Do nothing upon receiving an empty line."""
         pass
 
-    def do_EOF(self, line):
-        """
-
-        """
-        return True
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
     def do_quit(self, arg):
-        """
-
-        """
+        """Quit command to exit the program."""
         return True
-    
-    def do_create(self, arg):
-        """
-
-        """
-        commands = shlex.split(arg)
-
-        if len(commands) == 0:
-            print("** class name missing**")
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        else:
-            new_instance = eval(f"{commands[0]}()")
-            storage.save()
-            print(new_instance.id)
-    
-    def do_show(self, arg):
-        """
-
-        """
-        cammands = shlex.split(arg) # show User "12w-241"
-
-        if len(commands) == 0:
-            print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
-            print ("** class doesn't exist **")
-        elif len(commands) < 2:
-            print("** instance id missing **")
-        else:
-            objects = storage.all()
-
-            key = "{}.{}".format(commands[0], commands[1])
-            if key in objects:
-                print(objects[key])
-            else:
-                print("** no instance found **")
-
-
-    def do_destroy(self, arg):
-        """
-
-        """
-        commands = shlex.split(arg)
-
-        if len(commands) == 0:
-            print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif len(commands) < 2:
-            print("** instance id missing **")
-        else:
-            objects = storage.all()
-            key = "{}.{}".format(commands[0], commands[1])
-            if key in objects:
-                del objects[key]
-                storage.save()
-            else:
-                print("** no instance found **")
-
-
-    def do_all(self, arg):
-        """
-
-        """
-
-    objects = storage.all()
-
-    commands = shlex.split(arg)
-
-    if len(commands) == 0:
-        for key, value in objects.items():
-            print(str(value))
-    elif commands[0] not in self.valid_classes:
-        print("** class doesn't exist **")
-    else:
-        for key, value in objects.items():
-            if key.split('.')[0] == commands[0]:
-                print(str(value))
-
-    def default(self, arg):
-        """
-        Default behavior for cmd module when input is invalid
-            usage:
-                <class_name>.<method><("id",{"attr_name": "attr_value", "attr_name": "attr_value"}
-
-        """
-        # initail input
-        # User.update("87f12", {'first_name': "John", "age": 89})
-        arg_list = arg.split('.') # after split
-        # arg_list - ['User',("87f12", {'first_name': "John", "age": 89})
-        # arg_list[0] = 'User'
-        # arg_list[1] = 'update("87f12", {'first_name': "John", "age": 89})'
-
-    
-        # we save the arg_list index zero as incoming class name
-        incoming_class_name - arg_list[0]
-
-        # again we split arg_list[1] containing the method and attributes by opening brace
-        # to seperate the incoming method name and the accompanying arguments
-        command = arg_list[1].split('(') # after split
-        # command - ['update("87f12", {'first_name': "John", "age": 89})'
-        - command[0] = 'update'
-        # command[1] = '"87f12", {'first_name': "John", "age": 89})'
-        # we save the incoming method name as incoming method
-        incoming_method - command[0]  
-
-        # now we are left with the id, and dict with attr to update, at command index 1
-        # command[1] - "87f12", {'first_name': "John", "age": 89})
-        
-        incoming_xtra_arg - command[1].split(')')[0] # after split
-        # now, incoming_xtra_arg becomes the id, attr_name, and attr_value
-        # incoming_xtra_arg = "87f12", {'first_name': "John", "age": 89}'
-
-        # this part was for before the need to modify, when we were expecting
-        # User.update("id", attr_name, attr_value)
-
-        all_args = incoming_xtra_arg.split(',')
-        # so we had to further split by the comma, and pass the args to method
-        # construct for update.method. which gave us this:
-        # obj_id - all_args[0]
-        # attribute_name = all args[1]
-        # attribute_value - all_args[2]
-        # but will not work for this format, which is the current format:
-        # incoming_xtra_arg = '"87f12", {'first_name': "John", "age": 89})'
-        
-        method_dict - {
-                'all': self.do_all,
-                'show': self.do_show,
-                'destroy': self.do_destroy,
-                'update': self.do_update
-                'count': self.do_count
-                }
-
-        if incoming_method in method_dict.keys():
-            if incoming_method != "update":
-                return method_dict[incoming_method]("{} {}".format(incoming_class_name,
-                                                                    incoming_xtra_arg))
-            else:
-                obj_id, arg_dict = split_curly_braces(incoming_xtra_arg)
-                try:
-                    if isinstance(arg_dict, str):
-                        attributes = arg_dict
-                        return method_dict[incoming_method]("{} {} {}".format(incoming_class_name
-                                                                              attributes
-                                                                              obj_id))
-                    elif isinstance(arg_dict, str):
-                        dict_attributes = arg_dict
-                        return method_dict[incoming_method]("{} {} {}".format(incoming_class_name
-                                                                              obj_id
-                                                                              dict_attributes))
-                except Exception:
-                    print("** argument missing **")
-
-
-    print("*** Unknown syntax: {}".format(arg))
-    return False
-
-        def do_count(self, arg):
-            """
-
-            """
-            objects = storage.all()
-
-            # Use.count() City.count()
-            # count User or count City
-
-            commands = shlex.split(arg)  # ['User']
-            # commands[0] = 'User'
-            if arg:
-                incoming_class_name = commands[0]
-
-
-            count = 0
-
-            if incoming_class_name in self.valid_classes:
-                for obj in objects.values():
-                    if obj.__class__.__name__== incoming_class_name:
-                        count +- 1
-                    print(count)
-                else:
-                    print("** invalid class name **")
-            else:
-                print("** class name missing **")
-             
-
-
-        def do_update(self, arg):
-        """
-
-        """
-        commands = shlex.split(arg)
-
-        if len(commands) == 0:
-            print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif commands[0] < 2:
-            print("** instance id missing **")
-        else:
-            objects = storage.all()
-
-            key = "{}.{}".format(commands[0], commands[1])
-            if key not in objects:
-                print("** no instance found **")
-            elif len(commands) < 3:
-                print("** attribute name missing **")
-            elif len(commands) ,< 4:
-                print("** value missing **")
-            else:
-                obj = objects[key]
-                curly_braces = re.search(r"\{(.*?)\}", arg)
-
-                if curly_braces:
-                    str_data = curly_braces.group(1)
-
-                    arg_dict - ast.literal_eval("{" + str_data + "}")
-                    # {"first_name": "Johnson", "age": 490}
-
-                    attribute_names = list(arg_dict.keys())
-                    attribute_values = list(arg_dict.values())
-
-                    attr_name1 = attribute_names[0]
-                    attr_value1 = attribute_value[0]
-
-                    attr_name2 = attribute_names[1]
-                    attr_value2 = attribute_value[1]
-
-                    setattr(obj, attr_name1, attr_value1)
-                    setattr(obj, attr_name1, attr_value2)
-                else:
-
-                    
-                    attr_name = commands[2]
-                    attr_value = commands[3]
-
-                    try:
-                        attr_value = eva;(attr_value)
-                    except Exception:
-                         pass
-                    setatrr(obj, attr_name, attr_value)
-
-                obj.save()
-
-
-    def help_quit(self, arg):
-        """
-
-        """
-        print("Quit command to exit the program")
 
     def do_EOF(self, arg):
-        """
-
-        """
-        print()
+        """EOF signal to exit the program."""
+        print("")
         return True
+
+    def do_create(self, arg):
+        """Usage: create <class>
+        Create a new class instance and print its id.
+        """
+        argl = parse(arg)
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(argl[0])().id)
+            storage.save()
+
+    def do_show(self, arg):
+        """Usage: show <class> <id> or <class>.show(<id>)
+        Display the string representation of a class instance of a given id.
+        """
+        argl = parse(arg)
+        objdict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict:
+            print("** no instance found **")
+        else:
+            print(objdict["{}.{}".format(argl[0], argl[1])])
+
+    def do_destroy(self, arg):
+        """Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Delete a class instance of a given id."""
+        argl = parse(arg)
+        objdict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+            print("** no instance found **")
+        else:
+            del objdict["{}.{}".format(argl[0], argl[1])]
+            storage.save()
+
+    def do_all(self, arg):
+        """Usage: all or all <class> or <class>.all()
+        Display string representations of all instances of a given class.
+        If no class is specified, displays all instantiated objects."""
+        argl = parse(arg)
+        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            objl = []
+            for obj in storage.all().values():
+                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
+                    objl.append(obj.__str__())
+                elif len(argl) == 0:
+                    objl.append(obj.__str__())
+            print(objl)
+
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        argl = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
+
+    def do_update(self, arg):
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        argl = parse(arg)
+        objdict = storage.all()
+
+        if len(argl) == 0:
+            print("** class name missing **")
+            return False
+        if argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return False
+        if len(argl) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+            print("** no instance found **")
+            return False
+        if len(argl) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(argl) == 3:
+            try:
+                type(eval(argl[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+
+        if len(argl) == 4:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            if argl[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[argl[2]])
+                obj.__dict__[argl[2]] = valtype(argl[3])
+            else:
+                obj.__dict__[argl[2]] = argl[3]
+        elif type(eval(argl[2])) == dict:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            for k, v in eval(argl[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
+                else:
+                    obj.__dict__[k] = v
+        storage.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,119 +1,195 @@
 #!/usr/bin/python3
-"""
+"""Defines unittests for models/user.py.
 
+Unittest classes:
+    TestUser_instantiation
+    TestUser_save
+    TestUser_to_dict
 """
-
 import os
-import unittest
 import models
+import unittest
+from datetime import datetime
+from time import sleep
 from models.user import User
-from models.base_model import BaseModel
-
-class TestUser(unittest.TestCase):
 
 
+class TestUser_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the User class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(User, type(User()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(User(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(User().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(User().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(User().updated_at))
+
+    def test_email_is_public_str(self):
+        self.assertEqual(str, type(User.email))
+
+    def test_password_is_public_str(self):
+        self.assertEqual(str, type(User.password))
+
+    def test_first_name_is_public_str(self):
+        self.assertEqual(str, type(User.first_name))
+
+    def test_last_name_is_public_str(self):
+        self.assertEqual(str, type(User.last_name))
+
+    def test_two_users_unique_ids(self):
+        us1 = User()
+        us2 = User()
+        self.assertNotEqual(us1.id, us2.id)
+
+    def test_two_users_different_created_at(self):
+        us1 = User()
+        sleep(0.05)
+        us2 = User()
+        self.assertLess(us1.created_at, us2.created_at)
+
+    def test_two_users_different_updated_at(self):
+        us1 = User()
+        sleep(0.05)
+        us2 = User()
+        self.assertLess(us1.updated_at, us2.updated_at)
+
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        us = User()
+        us.id = "123456"
+        us.created_at = us.updated_at = dt
+        usstr = us.__str__()
+        self.assertIn("[User] (123456)", usstr)
+        self.assertIn("'id': '123456'", usstr)
+        self.assertIn("'created_at': " + dt_repr, usstr)
+        self.assertIn("'updated_at': " + dt_repr, usstr)
+
+    def test_args_unused(self):
+        us = User(None)
+        self.assertNotIn(None, us.__dict__.values())
+
+    def test_instantiation_with_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        us = User(id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(us.id, "345")
+        self.assertEqual(us.created_at, dt)
+        self.assertEqual(us.updated_at, dt)
+
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            User(id=None, created_at=None, updated_at=None)
+
+
+class TestUser_save(unittest.TestCase):
+    """Unittests for testing save method of the  class."""
+
+    @classmethod
     def setUp(self):
-        # Create a temporary test file for saving data
-        self.test_file = "test_file.json"
-        models.storage. file_path = self.test_file
-        models.storage.save()
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
 
     def tearDown(self):
-        # Remove the temporary test file after the test
-        if os.path.exists(self.test_file):
-            os.remove(self.test_file)
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
 
-    def test_user_attributes(self):
-        # Create a new User instance
-        test_user = user()
-        # Check if the default email attribute is an empty string
-        self.assertEqual(test_user.email, "")
-        # Check if the default password attribute is an empty string
-        self.assertEqual(test_user.password, "")
-        # Check if the default first_name attribute is an empty string
-        self.assertEqual(test_user.first_name, "")
-        # Check if the default last_name attributes is an empty string
-        self.assertEqual(test_user.last_name, "")
-    
-    def test_user_inherits_from_base_model(self):
-        # Create a User instance
-        test_user = User()
-        # Check if the User class is a subclass of BaseModel
-        self.assertTrue(issubclass(User, BaseModel))
+    def test_one_save(self):
+        us = User()
+        sleep(0.05)
+        first_updated_at = us.updated_at
+        us.save()
+        self.assertLess(first_updated_at, us.updated_at)
 
-    def test_user_str_representation(self):
-        # Create a new User instance
-        test_user = User()
-        # Set the attributes of the User instance
-        test_user.email = "Johnson@example.com"
-        test_user.first_name = "Johnson"
-        test_user.last_name = "Dennis"
-        test_user.password = "password123"
-        # Get the string representation of the User instance
-        user_str = str(test_user)
-        # Check if "User" is present in the string representation
-        self.assertIn("User", user_str)
-        # Check if the email is present in the string representation
-        self.assertIn("Johnson@example.com", user_str)
-        # Check if the first naem is present in the string representation
-        self.assertIn("Johnson", user_str)
-        # Check if the last time nameis present in the string representation
-        self.assertIn("Dennis", user_str)
+    def test_two_saves(self):
+        us = User()
+        sleep(0.05)
+        first_updated_at = us.updated_at
+        us.save()
+        second_updated_at = us.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        us.save()
+        self.assertLess(second_updated_at, us.updated_at)
 
-    def test_user_to_dict(self):
-        # Create a new User instance
-        test_user = User()
-        # Set the attributes of the User instance
-        test_user.email = "Johnson@example.com"
-        test_user.first_name = "Johnson"
-        test_user.last_name = "Dennis"
-        test_user.save()
-        # Get a dictionary representation of the User instance
-        user_dict = test_user.to_dict()
-        # Check if the 'email' key in the dictionary matches the set value
-        self.assertEqual(user_dict['email'], "Johnson@example.com")
-        # Check if the 'first_name' key in the dictionary matches the set value
-        self.assertEqual(user_dict['first_name'], "Johnson")
-        # Check if the 'last_name' key in the dictionary matches the set value
-        self.assertEqual(user_dict['last_name']," Dennis")
+    def test_save_with_arg(self):
+        us = User()
+        with self.assertRaises(TypeError):
+            us.save(None)
 
-    def test_user_instance_creation(self):
-        # Create a new User instance with arguments
-        test_user = User(email="Johnson@example.com", password="password123"
-                    first_name="Johnson", last_name="Dennis")
-        # Check if the 'email' attribute is set correct;y
-        self.assertEqual(test_user.email, "Johnson@example.com")
-        # Check if the 'password' attribute is set correctly
-        self.assertEqual(test_user.password, "password123")
-        # Check if the 'first_name' attribute is set correctly
-        self.assertFqual(test_user.first_name, "johnson")
-        # Check if the 'last_name' attribute is set correctly
-        self.assertEqual(test_user.last_name, "Dennis")
-
-    def test_user_instance_to_dict(self):
-        # Create a new User instane with specific attribute values
-        test_user = User(email="Johnson@example.com", password="password123"
-                    first_name="Johnson", last_name="Dennis")
-        # Convert the User instance too a dictonary
-        user_dict = test_user.to.dict()
-        # Check if the 'email' attribute is correctly represented in the dictionary
-        self.assertEqual(user_dict['email'], "Johnson@example.com")
-        # Check if the 'password' attribute is correctly represented in the dictionary
-        self.assertEqual(user_dict['password'], "password123")
-        # Check if the 'first_name'' attribute is correctly represented in the dictionary
-        self.assertEqual(user_dict['first_name'], "Johnson")
-        # Check if the 'last_time' attribute is correctly represented in the dictionary
-        self.assertEqual(user_dict['last_name'], "Dennis")
-
-    def test_user_id_generation(self):
-        # Create two different User instances
-        test_user = User()
-        user2 = User()
-        # Ensure that the 'id' attribute of each User instance is unique
-        self.assertNotEqual(test_user.id, user2.id)
+    def test_save_updates_file(self):
+        us = User()
+        us.save()
+        usid = "User." + us.id
+        with open("file.json", "r") as f:
+            self.assertIn(usid, f.read())
 
 
-if __name__ == '__main__':
+class TestUser_to_dict(unittest.TestCase):
+    """Unittests for testing to_dict method of the User class."""
+
+    def test_to_dict_type(self):
+        self.assertTrue(dict, type(User().to_dict()))
+
+    def test_to_dict_contains_correct_keys(self):
+        us = User()
+        self.assertIn("id", us.to_dict())
+        self.assertIn("created_at", us.to_dict())
+        self.assertIn("updated_at", us.to_dict())
+        self.assertIn("__class__", us.to_dict())
+
+    def test_to_dict_contains_added_attributes(self):
+        us = User()
+        us.middle_name = "Holberton"
+        us.my_number = 98
+        self.assertEqual("Holberton", us.middle_name)
+        self.assertIn("my_number", us.to_dict())
+
+    def test_to_dict_datetime_attributes_are_strs(self):
+        us = User()
+        us_dict = us.to_dict()
+        self.assertEqual(str, type(us_dict["id"]))
+        self.assertEqual(str, type(us_dict["created_at"]))
+        self.assertEqual(str, type(us_dict["updated_at"]))
+
+    def test_to_dict_output(self):
+        dt = datetime.today()
+        us = User()
+        us.id = "123456"
+        us.created_at = us.updated_at = dt
+        tdict = {
+            'id': '123456',
+            '__class__': 'User',
+            'created_at': dt.isoformat(),
+            'updated_at': dt.isoformat(),
+        }
+        self.assertDictEqual(us.to_dict(), tdict)
+
+    def test_contrast_to_dict_dunder_dict(self):
+        us = User()
+        self.assertNotEqual(us.to_dict(), us.__dict__)
+
+    def test_to_dict_with_arg(self):
+        us = User()
+        with self.assertRaises(TypeError):
+            us.to_dict(None)
+
+
+if __name__ == "__main__":
     unittest.main()
-        
-
